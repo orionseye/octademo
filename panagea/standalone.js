919d162e8983386f78137a3f5062d0fd0e5d61bf
Dimitrios Metoglou <sh22gr@gmail.com>
2025-09-27 21:32:55 +0200

    Upload files changed due to remake of standalone.js & Co
    Upload files changed due to remake of standalone.js & tours-grid-isotope.html & img folder (panagea/tours)
    Now logic works with images.json

--- panagea/standalone.js
+++ panagea/standalone.js
@@ -124,96 +124,120 @@ $(function() {
 	/**************************************************************************
 	[STEP TWO] a[data-lity] on click
 	*************************************************************************/
-	// 
-	
-	$("a[data-lity]").on('click', function() { // selector by attribute 'data-lity'
-	
-		$this = $( this );
-		$(".fotoX ul.slider-container").empty();// [0]
-		
-		var folder = "https://www.opencrs.cloud/octademo/img/panagea";
-		var file = $( this ).closest('.box_grid').find('figure img').attr('src').split('/')[2].split('.jpg')[0]; // [1]
-		
-		$.ajax({
-			url : folder,
-			success: function (data) {
-				$(data).find("a").attr("href", function (i, val) {
-					if( val.match('^' + file + '(.*)\.(jpe?g)$') ) { // [2]
-					$(".fotoX ul.slider-container").append("<li><img src='"+ folder + val +"'></li>"); // [3] swiffy-slider  (loading='lazy'  has bug..1st image gets not resized properly)
-					//$(".fotoX .slider-large").slick("slickAdd", "<div><img src='"+ folder + val +"'></div>");       // [3] for slick
-					console.log("<img src='"+ folder + val +"'>");
-					alert('done');
-					}
+$(function() { // must be inside doc ready
+
+    $("a[data-lity]").on('click', function() {
+
+        const $this = $(this);
+        const $sliderContainer = $(".fotoX ul.slider-container");
+        $sliderContainer.empty();
+
+        const folder = "img/panagea/tours/"; // adjust if needed
+
+        $.getJSON(folder + "images.json", function(data) {
+            let src = $this.closest('.box_grid').find('figure img').attr('src');
+            let file = src.split('/').pop().split('.jpg')[0]; // Extracts the filename without the extension (tour_a1, tour_b3, etc.), Used as the key to access images in images.json
+
+            if (!data[file] || data[file].length === 0) {
+                console.warn("⚠️ No images found in JSON for:", file);
+                return;
+            }
+
+			// preload first image
+			const firstImg = new Image();
+			firstImg.src = folder + data[file][0];
+
+			firstImg.onload = function() {
+
+				// (1) Append first image
+				$sliderContainer.append("<li class='active'><img src='" + firstImg.src + "' class='loaded'></li>");
+
+				// (2) Append remaining images
+				data[file].slice(1).forEach(img => {
+					$sliderContainer.append("<li><img src='" + folder + img + "' class='loaded'></li>");
 				});
-			},
-			complete: function (data) { //[4]
-				
+
+				// (3) Initialize or refresh Swiffy Slider safely
+				initSwiffySafe($sliderContainer);
+
+				// (4) OLD "complete" logic from previous script
 				//(1) populate each individual service rates & data
 				var walletid = $this.closest('.box_grid').attr("walletid");
 				var headertxt = $this.closest('.box_grid').find(".wrapper h3 a").text();
-				var adultrate  = parseInt($this.closest('.box_grid').find(".wrapper span.adultrate").text(), 10); //make sure it's a number
-				var chrate  = parseInt((adultrate/100)*60, 10); //simulate child rate as 60% of adult
+				var adultrate  = parseInt($this.closest('.box_grid').find(".wrapper span.adultrate").text(), 10);
+				var chrate  = parseInt((adultrate/100)*60, 10);
 				var servicetype = $this.closest('.box_grid').find("figure > small").text();
-				
-				$("#htmlDetail #service-header").text(headertxt);                  // set #htmlDetail service header
-				$(".pricingTable span.adrate").text(adultrate);                    // set #htmlDetail pricing table adult rate
-				$(".pricingTable span.chrate").text(chrate);                       // set #htmlDetail pricing table child rate
-				
-				$("#htmlDetail #service-header").attr('wallet-id',walletid);       // for wallet, set the service id
-				$("#htmlDetail #service-header").attr('service-type',servicetype); // for wallet, set the service type
-				$(".bookthis span.data-term").data('term',headertxt);              // for wallet, set the service name
-				$(".bookthis .adultsselect").data('price',adultrate);              // for wallet, set the adult rate
-				$(".bookthis .childrenselect").data('price',chrate);               // for wallet, set the child rate
-				
+
+				$("#htmlDetail #service-header").text(headertxt);
+				$(".pricingTable span.adrate").text(adultrate);
+				$(".pricingTable span.chrate").text(chrate);
+
+				$("#htmlDetail #service-header").attr('wallet-id', walletid);
+				$("#htmlDetail #service-header").attr('service-type', servicetype);
+				$(".bookthis span.data-term").data('term', headertxt);
+				$(".bookthis .adultsselect").data('price', adultrate);
+				$(".bookthis .childrenselect").data('price', chrate);
+
 				//(2) attach any extra services found
 				var optone = $this.closest('.box_grid').find(".opt-one");
 				var opttwo = $this.closest('.box_grid').find(".opt-two");
 				var addprefs = $this.closest('.box_grid').find(".add-prefs");
 
 				if(optone.length) {
-					
 					$('#optone').clone().appendTo('.stdaloneDetailOptions');
-					
-					$('#optone p.clone-title').text( optone.data('test').term );
-					$('#optone span.data-term').data('term', optone.data('test').term );
-					
-					$('#optone div.clone-adrate').text( optone.data('test').adrate+',00 €' );
-					$('#optone .adultsselect').data('price', optone.data('test').adrate );
-					
-					$('#optone div.clone-chrate').text( optone.data('test').chrate+',00 €' );
-					$('#optone .childrenselect').data('price', optone.data('test').chrate );
-					
+					$('#optone p.clone-title').text(optone.data('test').term);
+					$('#optone span.data-term').data('term', optone.data('test').term);
+					$('#optone div.clone-adrate').text(optone.data('test').adrate+',00 €');
+					$('#optone .adultsselect').data('price', optone.data('test').adrate);
+					$('#optone div.clone-chrate').text(optone.data('test').chrate+',00 €');
+					$('#optone .childrenselect').data('price', optone.data('test').chrate);
 				}
+
 				if(opttwo.length) {
-					
 					$('#opttwo').clone().appendTo('.stdaloneDetailOptions');
-					
-					$('#opttwo p.clone-title').text( opttwo.data('test').term );
-					$('#opttwo span.data-term').data('term', opttwo.data('test').term );
-					
-					$('#opttwo div.clone-adrate').text( opttwo.data('test').adrate+',00 €' );
-					$('#opttwo .adultsselect').data('price', opttwo.data('test').adrate );
-					
-					$('#opttwo div.clone-chrate').text( opttwo.data('test').chrate+',00 €' );
-					$('#opttwo .childrenselect').data('price', opttwo.data('test').chrate );
-					
+					$('#opttwo p.clone-title').text(opttwo.data('test').term);
+					$('#opttwo span.data-term').data('term', opttwo.data('test').term);
+					$('#opttwo div.clone-adrate').text(opttwo.data('test').adrate+',00 €');
+					$('#opttwo .adultsselect').data('price', opttwo.data('test').adrate);
+					$('#opttwo div.clone-chrate').text(opttwo.data('test').chrate+',00 €');
+					$('#opttwo .childrenselect').data('price', opttwo.data('test').chrate);
 				}
+
 				if(addprefs.length) {
 					$('.clone-preferences').clone().appendTo('.stdaloneDetailOptions');
 				}
 
 				//(3) populate the selects options values
-				//set option values of each select taken from its attr data-price
 				$('.adultsselect, .childrenselect').each(function() {
-					let inival = parseInt($( this ).data('price'), 10); // cache the attr data-price + make sure it's an integer
-					//console.log(inival);
-					
-					// find the select's (each) option with .map >> then multiply its own value by initial price (e.g value "1" * inival) >> .. then set it as new value
-					$( this ).find('option').map(function() { return $(this).val( $(this).val()*inival ); }); 
+					let inival = parseInt($(this).data('price'), 10);
+					$(this).find('option').map(function() { return $(this).val($(this).val()*inival); });
 				});
-			}
-		});
-	});	
+
+			};
+
+        }).fail(() => {
+            console.error("⚠️ Failed to load images.json from", folder + "images.json");
+        });
+
+    });
+
+}); // doc ready
+
+/*
+ * Initializes or refreshes the SwiffySlider safely using initSwiffySafe(), 
+ * which ensures the SwiffySlider library is defined before initialization.
+ */
+function initSwiffySafe($container) {
+    if (typeof SwiffySlider !== "undefined") {
+        if ($container[0].swiffySlider) {
+            $container[0].swiffySlider.refresh();
+        } else {
+            new SwiffySlider($container[0]);
+        }
+    } else {
+        setTimeout(() => initSwiffySafe($container), 50);
+    }
+}
 	
 	
 /****************************************************************************************
@@ -267,30 +291,35 @@ which is needed to identify each service id in our wallet(see data_wallet_id)
 Then, on Ajax [STEP TWO], we pass this id to .data("wallet-id")
 
 
-
 [STEP TWO]  a[data-lity] on click
 Before lity opens, we need to (A) load images & (B) process service data
 
-(A) populate the slider images into the slider container
-    Credit https://stackoverflow.com/a/32940532/801018
+(A)  
+ * Handle product image clicks for the lightbox/slider.
+ *
+ * This click handler applies to all <a> elements with the 'data-lity' attribute.
+ * When a product is clicked, it:
+ *   1. Clears the slider container (.fotoX ul.slider-container) of previous images.
+ *   2. Determines the base filename of the clicked product's image.
+ *   3. Loads the corresponding image set from images.json.
+ *   4. Preloads the first image and appends it immediately to the slider (with 'loaded' class for fade-in).
+ *   5. Appends remaining images to the slider.
+ *   6. Initializes or refreshes the SwiffySlider safely using initSwiffySafe(), 
+ *      which ensures the SwiffySlider library is defined before initialization.
+ *
+ * Advantages of this approach:
+ *   - Works reliably both locally and on GitHub Pages.
+ *   - Supports any number of images per product.
+ *   - Ensures smooth fade-in effect for all images.
+ *   - Prevents SwiffySlider undefined errors due to script timing issues.
+ * 
+ * HTML requirement:
+ *   - Each product <a> must have a 'data-lity' attribute.
+ *   - The slider container must be present in the DOM: .fotoX ul.slider-container
 
-	[0] first remove all previous images (holded in div) / 
-		for slick use .slick("slickRemove", "div");
-
-	[1] 1st split path '/' and get the img name [2] .. 
-		then split the extension '.jpg' and get the pure file name [0]
-		
-	[2] matches ^tour_1                https://stackoverflow.com/a/7879009/801018  
-	  + matches everything after (.*)  https://stackoverflow.com/a/4419021/801018
-		to find all images   if( val.match(/\.(jpe?g|png|gif)$/) )
-			
-	[3] For slick carousel:
-		use .slick("slickAdd", value) --> NOT .append( ..bla bla)
-		"slickAdd" because there is still some JS magic slick need to call 
-		before adding an item. .append does not call those.
-
-(B) we populate individual id's, rates etc to their respective fields
-	so we can process each service with tis own values
+(B) 
+we populate individual id's, rates etc to their respective fields
+so we can process each service with tis own values
 	
 	
 [STEP THREE] .on('lity:close')
