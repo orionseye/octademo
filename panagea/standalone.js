97978ad97aa7664268d9847eec5555d0748c72b6
Dimitrios Metoglou <sh22gr@gmail.com>
2025-09-27 17:39:36 +0200

    Update standalone.js

--- panagea/standalone.js
+++ panagea/standalone.js
@@ -131,7 +131,7 @@ $(function() {
 		$this = $( this );
 		$(".fotoX ul.slider-container").empty();// [0]
 		
-		var folder = "img/";
+		var folder = "panagea/img/";
 		var file = $( this ).closest('.box_grid').find('figure img').attr('src').split('/')[2].split('.jpg')[0]; // [1]
 		
 		$.ajax({
