244b494ff2106204c88476778a0e79ed30558c25
Dimitrios Metoglou <sh22gr@gmail.com>
2025-09-27 15:56:40 +0200

    added file .nojekyll and minor changes
    added file .nojekyll to disable pages being generated with jekyll

--- GTrip/js/main.js
+++ GTrip/js/main.js
@@ -641,7 +641,7 @@ function initMap() {
 
   const contentString = `
     <div class="mapItem d-flex">
-      <img src="_GTrip/img/lists/hotel/1/1.png" alt="image" class="mapItem__img size-100 rounded-4">
+      <img src="GTrip/img/lists/hotel/1/1.png" alt="image" class="mapItem__img size-100 rounded-4">
 
       <div class="mapItem__content d-flex flex-column justify-between ml-15">
         <div>
